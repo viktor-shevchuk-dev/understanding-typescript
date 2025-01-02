374f653dfe0c0c7c1ae960a1d2cade5557336651
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-01-02 17:59:52 +0100

    add advanced ts features

--- index.js
+++ index.js
@@ -1,39 +1,91 @@
 "use strict";
-let add;
-add = (n1, n2) => {
-    return n1 + n2;
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+const e1 = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
 };
-add(1, 2);
-class Person {
-    constructor(n) {
-        this.age = 30;
-        if (n) {
-            this.name = n;
-        }
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
     }
-    greet(phrase) {
-        if (this.name) {
-            console.log(phrase + ' ' + this.name);
-        }
-        else {
-            console.log(phrase);
-        }
+    return a + b;
+}
+const result = add(1, 2);
+result.toFixed();
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Viktor',
+    job: { title: 'CEO', description: 'My own company' },
+};
+console.log(`I am a ${(_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title} of ${(_b = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _b === void 0 ? void 0 : _b.description}.`);
+const userInput = null;
+const storedData = userInput !== null && userInput !== void 0 ? userInput : 'Default value';
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
     }
 }
-let user1;
-user1 = new Person();
-// user1 = {
-//   name: 'John',
-//   age: 12,
-//   greet(phrase) {
-//     console.log(phrase + ' ' + this.name);
-//   },
-// };
-if (user1.greet) {
-    user1.greet('Hi');
+printEmployeeInformation({
+    name: 'Max',
+    startDate: new Date(),
+});
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
 }
-else {
-    console.log('No greet method defined.');
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo...' + amount);
+    }
 }
-// user1.name = '';
-console.log(user1);
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // if ('loadCargo' in vehicle) {
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+useVehicle(v1);
+useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log(animal.type + ' is moving at speed ' + speed);
+}
+moveAnimal({ type: 'horse', runningSpeed: 12 });
+const jsdom_1 = require("jsdom");
+const dom = new jsdom_1.JSDOM('<!DOCTYPE html><p></p>');
+const document = dom.window.document;
+const nodeRefs = { input: document.createElement('input') };
+nodeRefs.input.id = 'user-input';
+document.body.appendChild(nodeRefs.input);
+const browserRefs = {
+    // input: <HTMLInputElement>document.getElementById('user-input'),
+    input: document.getElementById('user-input'),
+};
+console.log(browserRefs.input);
+browserRefs.input.value = '';
+const errorBag = {
+    id: 'email1',
+    email: 'Not a valid email',
+    userName: 'Must start with a capital character',
+};
--- index.ts
+++ index.ts
@@ -1,62 +1,144 @@
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
-}
+type Admin = { name: string; privileges: string[] };
 
-let add: AddFn;
+type Employee = { name: string; startDate: Date };
 
-add = (n1, n2) => {
-  return n1 + n2;
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
 };
 
-add(1, 2);
+type Combinable = string | number;
+type Numeric = number | boolean;
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
+type Universal = Combinable & Numeric;
 
-interface Aged {
-  age: number;
-}
-
-interface Greetable extends Named {
-  greet?(phrase: string): void;
-}
-
-class Person implements Greetable, Aged {
-  name?: string;
-  age = 30;
-
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
   }
 
-  greet(phrase: string): void {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log(phrase);
-    }
+  return a + b;
+}
+
+const result = add(1, 2);
+result.toFixed();
+
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Viktor',
+  job: { title: 'CEO', description: 'My own company' },
+};
+console.log(
+  `I am a ${fetchedUserData?.job?.title} of ${fetchedUserData?.job?.description}.`
+);
+
+const userInput = null;
+const storedData = userInput ?? 'Default value';
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
   }
 }
 
-let user1: Greetable;
-user1 = new Person();
-// user1 = {
-//   name: 'John',
-//   age: 12,
-//   greet(phrase) {
-//     console.log(phrase + ' ' + this.name);
-//   },
-// };
+printEmployeeInformation({
+  name: 'Max',
+  startDate: new Date(),
+});
 
-if (user1.greet) {
-  user1.greet('Hi');
-} else {
-  console.log('No greet method defined.');
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
 }
-// user1.name = '';
-console.log(user1);
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // if ('loadCargo' in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+const v1 = new Car();
+const v2 = new Truck();
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+type Animal = Bird | Horse;
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log(animal.type + ' is moving at speed ' + speed);
+}
+
+moveAnimal({ type: 'horse', runningSpeed: 12 });
+
+import { JSDOM } from 'jsdom';
+const dom = new JSDOM('<!DOCTYPE html><p></p>');
+const document = dom.window.document;
+
+const nodeRefs = { input: document.createElement('input') };
+nodeRefs.input.id = 'user-input';
+document.body.appendChild(nodeRefs.input);
+
+const browserRefs = {
+  // input: <HTMLInputElement>document.getElementById('user-input'),
+  input: document.getElementById('user-input') as HTMLInputElement,
+};
+console.log(browserRefs.input);
+browserRefs.input.value = '';
+
+interface ErrorContainer {
+  [key: string]: string;
+  id: string; // the same type as the index property only!
+}
+
+const errorBag: ErrorContainer = {
+  id: 'email1',
+  email: 'Not a valid email',
+  userName: 'Must start with a capital character',
+};
